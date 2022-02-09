7d37625a8ac50cfbfb92d828d3f9a1fa64b23f9d
nicolewallace09 <nicole.elisaw@gmail.com>
2022-02-09 13:43:40 -0800

    added new projects

--- components/Projects.js
+++ components/Projects.js
@@ -2,46 +2,51 @@ import React from 'react';
 
 const userData = {
     projects: [
-        {
+          {
             id: 1, 
+            title: "Exchange Rates",
+            link: "https://competent-kilby-6c3bd3.netlify.app/",
+            imgUrl: "https://github.com/nicolewallace09/exchange-rates/raw/main/src/images/home.png",
+            repo: "https://github.com/nicolewallace09/exchange-rates"
+        },
+        {
+            id: 2, 
             title: "Movie Search App",
             link: "https://nicolewallace09.github.io/react-movie-app",
             imgUrl: "https://github.com/nicolewallace09/react-movie-app/raw/main/public/images/search.png",
             repo: "https://github.com/nicolewallace09/react-movie-app"
         },
         {
-            id: 2, 
+            id: 3, 
             title: "Student Profiles",
             link: "https://nicolewallace09.github.io/student-profiles/",
             imgUrl: "https://github.com/nicolewallace09/student-profiles/raw/main/public/images/app.png",
             repo: "https://github.com/nicolewallace09/student-profiles"
         },
         {
-            id: 3, 
+            id: 4, 
             title: "The Shoppies",
             link: "https://nicolewallace09.github.io/the-shoppies/",
             imgUrl: "https://github.com/nicolewallace09/the-shoppies/raw/main/public/images/screenshot.png",
             repo: "https://github.com/nicolewallace09/the-shoppies"
         },
         {
-            id: 4, 
+            id: 5, 
+            title: "Employee Tracker",
+            imgUrl: "ProjectImages/employe-tracker.png",
+            repo: "https://github.com/nicolewallace09/employee-tracker"
+        },
+        {
+            id: 7, 
             title: "Web Scraper",
             imgUrl: "ProjectImages/webscraper.png",
             repo: "https://github.com/nicolewallace09/webscraper2"
         },
         {
-            id: 5, 
-            title: "Travel Planner",
-            link: "https://nicolewallace09.github.io/travel-planner/",
-            imgUrl: "https://github.com/izabelacloud/Travel-Planner/raw/master/assets/images/App1.png?raw=true",
-            repo: "https://github.com/nicolewallace09/travel-planner"
-        },
-        {
-            id: 6, 
-            title: "Techly",
-            link: "https://techly.herokuapp.com/",
-            imgUrl: "https://github.com/nicolewallace09/techly/raw/master/assets/images/screenshot.png",
-            repo: "https://github.com/nicolewallace09/techly"
+            id: 8, 
+            title: "Readme Generator",
+            imgUrl: "https://github.com/nicolewallace09/readme-generator/raw/master/utils/README-GENERATOR%20(1).gif",
+            repo: "https://github.com/nicolewallace09/readme-generator"
         },
         {
             id: 7, 
@@ -50,35 +55,7 @@ const userData = {
             repo: "https://github.com/nicolewallace09/team-profile-generator"
         },
         {
-            id: 8,
-            title: "Work Day Scheduler",
-            link: "https://nicolewallace09.github.io/work-day-scheduler/",
-            imgUrl: "https://github.com/nicolewallace09/work-day-scheduler/raw/master/assets/images/calender-shot.png",
-            repo: "https://github.com/nicolewallace09/work-day-scheduler"
-        },
-        {
             id: 9,
-            title: "Weather App",
-            link: "https://nicolewallace09.github.io/weather-dashboard/",
-            imgUrl: "https://github.com/nicolewallace09/weather-dashboard/raw/master/assets/images/weather-img.png",
-            repo: "https://github.com/nicolewallace09/weather-dashboard"
-        },
-        {
-            id: 10,
-            title: "The Tech Blog",
-            link: "https://dry-harbor-54501.herokuapp.com/",
-            imgUrl: "https://github.com/nicolewallace09/the-tech-blog/raw/master/assets/images/screen.png",
-            repo: "https://github.com/nicolewallace09/the-tech-blog"
-        },
-        {
-            id: 11,
-            title: "Note Taker",
-            link: "https://note-express.herokuapp.com/",
-            imgUrl: "https://github.com/nicolewallace09/note-taker/raw/master/image/screenshot.gif",
-            repo: "https://github.com/nicolewallace09/note-taker"
-        },
-        {
-            id: 12,
             title: "E-Commerce Backend",
             imgUrl: "ProjectImages/ecommerce.png",
             repo: "https://github.com/nicolewallace09/e-commerce-backend"
